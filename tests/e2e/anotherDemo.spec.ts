96a28d5bb14fa9d4497a3da1c5c7b4bd7c7f0c6d
LawrenceZ <LawrenceZ@users.noreply.github.com>
2023-05-01 15:24:06 -0400

    add POM test fixtures

--- tests/e2e/anotherDemo.spec.ts
+++ tests/e2e/anotherDemo.spec.ts
@@ -1,24 +1,22 @@
-import { test, expect } from "@playwright/test";
 import { Logger } from "../../utls";
-import { InventoryPage, CartPage } from "../pages";
+import { test } from "../helper/baseTest";
 
 const logger = new Logger({ logName: "anotherDemo.spec" });
 test.describe("Another Demo purposes", () => {
-  let inventoryPage: InventoryPage;
-  let cartPage: CartPage;
-
-  test.beforeEach(async ({ page }) => {
-    inventoryPage = new InventoryPage(page);
-    cartPage = new CartPage(page);
-  });
-
-  test("should be able to check with items in cart @smoke", async () => {
+  test("should be able to check with items in cart @smoke", async ({
+    inventoryPage,
+    cartPage,
+  }) => {
     logger.info("this is a demo test to checkout cart");
-    await inventoryPage.navigateTo();
-    await inventoryPage.expect.toHaveInventoryList();
-    await inventoryPage.addToCartByItem("Sauce Labs Bike Light");
-    await inventoryPage.openShippingCart();
-    await cartPage.expect.toBeOnYourCartPage();
-    await cartPage.checkout();
+    await test.step("add to cart", async () => {
+      await inventoryPage.navigateTo();
+      await inventoryPage.expect.toHaveInventoryList();
+      await inventoryPage.addToCartByItem("Sauce Labs Bike Light");
+    });
+    await test.step("checkout cart", async () => {
+      await inventoryPage.openShippingCart();
+      await cartPage.expect.toBeOnYourCartPage();
+      await cartPage.checkout();
+    });
   });
 });
--- tests/e2e/demo.spec.ts
+++ tests/e2e/demo.spec.ts
@@ -1,28 +1,28 @@
-import { test, expect } from "@playwright/test";
 import { Logger } from "../../utls";
-import { InventoryPage } from "../pages";
+import { test } from "../helper/baseTest";
 
 const logger = new Logger({ logName: "demo.spec" });
 test.describe("Demo purposes", () => {
-  let inventoryPage: InventoryPage;
-
-  test.beforeEach(async ({ page }) => {
-    inventoryPage = new InventoryPage(page);
+  test.beforeEach(async ({ inventoryPage }) => {
     await inventoryPage.navigateTo();
     await inventoryPage.expect.toHaveInventoryList();
   });
 
-  test("should be able to logout from inventry page", async () => {
+  test("should be able to logout from inventry page", async ({
+    inventoryPage,
+  }) => {
     logger.info("this is a demo test to login and then to logout");
     await inventoryPage.logout();
   });
 
-  test("should be able to add item to cart", async () => {
+  test("should be able to add item to cart", async ({ inventoryPage }) => {
     logger.info("this is a demo test to add item to cart");
     await inventoryPage.addToCartByItem("Sauce Labs Bike Light");
   });
 
-  test("should be able to remove item to cart @smoke", async () => {
+  test("should be able to remove item to cart @smoke", async ({
+    inventoryPage,
+  }) => {
     logger.info(
       "this is a demo test to remove item after add the same item to cart"
     );
